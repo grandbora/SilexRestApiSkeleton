ae8d20641a6bd3f5637e2ce18649dde34fcb9a7d
grandbora <grandbora@yahoo.com>
2012-07-02 17:41:56 +0200

    init

--- web/index.php
+++ web/index.php
@@ -1,10 +1,16 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
 $app = new Silex\Application();
+$app['debug'] = true;
 
 $app->get('/asd', function() use($app) {
             echo "asd";
+        });
+
+$app->get('/', function() use($app) {
+            echo "ROOT WOOT";
         });
 
 $app->run();
